47d7b5e473d0548e0f336feb866e46997ffd8481
develaper <lachguer.brahim@gmail.com>
2016-09-16 16:56:04 +0200

    Switch root's route to the hq version

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Osra::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
 
-  root to: "admin/dashboard#index"
+  root to: "hq/dashboard#index"
 
   #build a new sponsorship on the Orphan class instead of the Sponsorship class
   get '/admin/sponsors/:sponsor_id/sponsorships/new', to: 'admin/orphans#index', as: :new_sponsorship
